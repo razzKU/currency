d86c853a268b19b0b85048f20820a584920f1fa2
Razeena C P <116195725+razzKU@users.noreply.github.com>
2022-10-21 19:28:25 +0530

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,25 @@
+
+limit = int(input("Enter the number of coins: "))
+coins = []
+for i in range(limit):
+    coins.append(int(input(f"Enter the value of coin{i+1}: ")))
+
+coins.sort()
+print(f"Coins you entered is: {coins}")
+note = int(input("Enter the value of currency note: "))
+count = limit - 1
+flag = 0
+print()
+while note != 0:
+    denominator = coins[count]
+    value = note / denominator
+    if value >= 1:
+        print(f"{denominator} * {int(value)} coins")
+    note = note % denominator
+    flag = flag + int(value)
+    count = count - 1
+    if count < 0 and note is not 0:
+        print(f"The remaining Currency value: {note}")
+        break
+
+print(f"\nNumber of minimum coin needed: {flag}")
