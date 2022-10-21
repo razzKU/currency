31508084687b8fa41fd5d46bfaa523534e8298e7
Razeena C P <116195725+razzKU@users.noreply.github.com>
2022-10-21 23:29:33 +0530

    Update currency.py

--- currency.py
+++ currency.py
@@ -10,16 +10,39 @@ note = int(input("Enter the value of currency note: "))
 count = limit - 1
 flag = 0
 print()
-while note != 0:
-    denominator = coins[count]
-    value = note / denominator
-    if value >= 1:
-        print(f"{denominator} * {int(value)} coins")
-    note = note % denominator
-    flag = flag + int(value)
-    count = count - 1
-    if count < 0 and note is not 0:
-        print(f"The remaining Currency value: {note}")
+for i in range(limit - 1, -1, -1):
+    if note % coins[i] == 0:
+        flag = int(note / coins[i])
+        print(f"{coins[i]} * {flag} coins")
         break
 
+if flag == 0:
+    while note != 0:
+        denominator = coins[count]
+        value = note / denominator
+        if value >= 1:
+            print(f"{denominator} * {int(value)} coins")
+        note = note % denominator
+        flag = flag + int(value)
+        count = count - 1
+        if count < 0 and note is not 0:
+            print(f"The remaining Currency value: {note}")
+            break
+
 print(f"\nNumber of minimum coin needed: {flag}")
+
+
+
+# while note != 0:
+#     denominator = coins[count]
+#     value = note / denominator
+#     if value >= 1:
+#         print(f"{denominator} * {int(value)} coins")
+#     note = note % denominator
+#     flag = flag + int(value)
+#     count = count - 1
+#     if count < 0 and note is not 0:
+#         print(f"The remaining Currency value: {note}")
+#         break
+#
+# print(f"\nNumber of minimum coin needed: {flag}")
